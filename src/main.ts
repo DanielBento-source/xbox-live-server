a30fa0f71abf492598225c7f1ac946397910478d
DanielBento <dsbld2@gmail.com>
2022-06-03 13:38:34 -0300

    chore(main): add 'process.env.PORT'

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3633);
+  await app.listen(process.env.PORT || 3633);
 }
 bootstrap();
